0dfd29db9ab5becde933a50bc23880bdf3864f4e
bw02184 <bw02184@naver.com>
2024-07-26 16:20:01 +0900

    components dropdown-menue

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,193 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Menue from "./components/dropdown-menue/Menue";
+import "./App.css";
+
+let icons = [
+  {
+    id: self.crypto.randomUUID(),
+    url: "https://www.naver.com",
+    src: "../images/youtube.png",
+    name: "이름",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    url: "https://google.com",
+    src: "../images/youtube.png",
+    name: "이름",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    url: "https://google.com",
+    src: "../images/youtube.png",
+    name: "이름",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    url: "https://google.com",
+    src: "../images/youtube.png",
+    name: "이름",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    url: "https://google.com",
+    src: "../images/youtube.png",
+    name: "이름",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    url: "https://google.com",
+    src: "../images/youtube.png",
+    name: "이름",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    url: "https://google.com",
+    src: "../images/youtube.png",
+    name: "이름",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    url: "https://google.com",
+    src: "../images/youtube.png",
+    name: "이름",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    url: "https://google.com",
+    src: "../images/youtube.png",
+    name: "이름",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    url: "https://google.com",
+    src: "../images/youtube.png",
+    name: "이름",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    url: "https://google.com",
+    src: "../images/youtube.png",
+    name: "이름",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    url: "https://google.com",
+    src: "../images/youtube.png",
+    name: "이름",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    url: "https://google.com",
+    src: "../images/youtube.png",
+    name: "이름",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    url: "https://google.com",
+    src: "../images/youtube.png",
+    name: "이름",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    url: "https://google.com",
+    src: "../images/youtube.png",
+    name: "이름",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    url: "https://google.com",
+    src: "../images/youtube.png",
+    name: "이름",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    url: "https://google.com",
+    src: "../images/youtube.png",
+    name: "이름",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    url: "https://google.com",
+    src: "../images/youtube.png",
+    name: "이름",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    url: "https://google.com",
+    src: "../images/youtube.png",
+    name: "이름",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    url: "https://google.com",
+    src: "../images/youtube.png",
+    name: "이름",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    url: "https://google.com",
+    src: "../images/youtube.png",
+    name: "이름",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    url: "https://google.com",
+    src: "../images/youtube.png",
+    name: "이름",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    url: "https://google.com",
+    src: "../images/youtube.png",
+    name: "이름",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    url: "https://google.com",
+    src: "../images/youtube.png",
+    name: "이름",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    url: "https://google.com",
+    src: "../images/youtube.png",
+    name: "이름",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    url: "https://google.com",
+    src: "../images/youtube.png",
+    name: "이름",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    url: "https://google.com",
+    src: "../images/youtube.png",
+    name: "이름",
+  },
+  {
+    id: self.crypto.randomUUID(),
+    url: "https://google.com",
+    src: "../images/youtube.png",
+    name: "이름",
+  },
+];
+
+const positionIcons = (icons, number) => {
+  if (icons.length % number !== 0) {
+    for (let i = 0; i < icons.length % 3; i++) {
+      icons.push({ id: self.crypto.randomUUID() });
+    }
+  }
+  return icons;
+};
+icons = positionIcons(icons, 3);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Menue icons={icons} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/dropdown-menue/Icon.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Icon.css";
+
+const Icon = ({ url, src, name }) => {
+  return (
+    <a className="image-box" href={url}>
+      <img src={src} alt="" width={30} height={30} style={{ border: "none" }} />
+      <span className="image-name">{name}</span>
+    </a>
+  );
+};
+
+export default Icon;
