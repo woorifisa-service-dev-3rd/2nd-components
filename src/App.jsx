5ee7ed0b62f76d32566d90a12ef092b61de5eb5a
Kee0304 <kee0304@gmail.com>
2024-07-26 16:36:38 +0900

    [FE] route: 테스트 페이지 분리

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import { Link, Route, Routes } from 'react-router-dom'
 import './App.css'
 import TestPage from './TestPages/TestPage'
 import TestHome from './TestPages/TestHome'
-function App() {
-
+import Menue from './components/dropdown-menue/Menue'
 
 let icons = [
   {
@@ -195,12 +194,13 @@ function App() {
       <div>
         <Link to="/">홈으로</Link>
         <Link to="/testpage">테스트 페이지로</Link>
+        <Link to="/testmenu">테스트 메뉴로</Link>
         <Routes>
           <Route path="/" element={<TestHome/>}></Route>
           <Route path="/testpage" element={<TestPage/>}></Route>
+          <Route path='/testmenu' element={<Menue icons ={icons}/>}></Route>
         </Routes>
       </div>
-      <Menue icons={icons} />
     </>
   );
 }
